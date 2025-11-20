ebaec7a8851e2f32566eeaa879d79cfaf7192e64
YuriAbbud <yuriabbud@gmail.com>
2025-11-20 16:11:00 -0300

    Implementação histórico de conversa

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://github.com/YuriAbbud/chatbot-api
+
 https://gemini.google.com/share/8a38cc3ee715
 
 Logging em Python: como escrever logs como um profissional!: https://www.youtube.com/watch?v=pxuXaaT1u3k
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from langchain_ollama import OllamaLLM
-from langchain_core.prompts import PromptTemplate
-from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI, Header
 from pydantic import BaseModel
 import time
 import psutil
 import logging
+import uuid
+
+historico_sessoes = {}
 
 class ChatRequest(BaseModel):
     mensagem: str
 
 class ChatResponse(BaseModel):
-    resposta: str
+   resposta: str
+   session_id: str
+
+template = """
+CONTEXT:
+{contexto}
+
+USER: {mensagem}
+IA:
+"""
 
 llm = OllamaLLM(model="llama3.2:1b")
-prompt = PromptTemplate.from_template("{mensagem}")
+prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | llm
 
-def obter_resposta(mensagem_usuario: str) -> str:
-    resposta = chain.invoke({"mensagem": mensagem_usuario})
+def obter_resposta(contexto: str, mensagem_usuario: str) -> str:
+    resposta = chain.invoke({"contexto": contexto, "mensagem": mensagem_usuario})
     return resposta
 
 app = FastAPI(
@@ -25,24 +37,30 @@ app = FastAPI(
 )
 
 @app.post("/chat", response_model=ChatResponse)
-def chat_endpoint(request: ChatRequest):
+def chat_endpoint(request: ChatRequest, session_id: str = Header(None, alias="Session-id")):
+
+    if session_id is None:
+        session_id = str(uuid.uuid4())
+        historico_sessoes[session_id] = []
+    else:
+        if session_id not in historico_sessoes:
+            historico_sessoes[session_id] = []
+
 
     start = time.time()
 
     mensagem = request.mensagem
+    historico = historico_sessoes[session_id]
+    contexto = "\n".join(historico)
 
-    resposta = obter_resposta(mensagem)
+    resposta = obter_resposta(contexto, mensagem)
+
+    historico.append(f"USER: {mensagem}")
+    historico.append(f"IA: {resposta}")
 
     duration = time.time() - start
+    milis = int((duration % 1) * 1000)
     duration_formated = time.strftime("%H:%M:%S", time.gmtime(duration))
-    print(f"Requisicao processada em {duration_formated} segundos.")
+    print(f"Requisicao processada em {duration_formated}.{milis:03d}")
     
-    return {"resposta":resposta}
-
-@app.get("/metrics")
-def get_metrics():
-    return {
-        "cpu_usage": f"{psutil.cpu_percent()}%",
-        "memory_usage": f"{psutil.virtual_memory().percent}%"
-    }
-
+    return {"resposta":resposta,"session_id":session_id}
